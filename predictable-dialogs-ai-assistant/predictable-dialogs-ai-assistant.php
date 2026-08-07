9a8c3007552d93045e0560f97bc8965644987e64
dev <jaikant@gmail.com>
2026-08-07 13:22:27 +0530

    updated Author URI

--- predictable-dialogs-ai-assistant/predictable-dialogs-ai-assistant.php
+++ predictable-dialogs-ai-assistant/predictable-dialogs-ai-assistant.php
@@ -6,7 +6,7 @@
  * Version: 0.1.0
  * Requires at least: 6.0
  * Requires PHP: 7.4
- * Author: jai@predictabledialogs.com jaikant@gmail.com
+ * Author: jai@predictabledialogs.com
  * Author URI: https://github.com/Predictable-Dialogs
  * License: GPL v2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
